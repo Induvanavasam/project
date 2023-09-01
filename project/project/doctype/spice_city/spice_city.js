7059bae6c9e25ac283b5d2ddd78b18d3293e8037
sreejani-vankayala <sreejanivankayala@gmail.com>
2023-09-01 09:49:34 +0000

    spice city

---
+++ project/project/doctype/spice_city/spice_city.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, indu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Spice City', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ project/project/doctype/spice_city/test_spice_city.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, indu and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSpiceCity(FrappeTestCase):
+	pass
